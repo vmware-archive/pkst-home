4b10486af359f3d69ecd83794c63f0545c72d957
Fei Yang <fyang@pivotal.io>
2020-01-03 10:25:25 -0800

    Move renew-env pipeline to hush-house
    [#169791989]
    
    Co-authored-by: Fei Yang <fyang@pivotal.io>
    Co-authored-by: Tanay Kothari <tanayk2610@gmail.com>

--- renew-env/ci/set_pipeline.sh
+++ renew-env/ci/set_pipeline.sh
@@ -9,7 +9,7 @@ GITHUB_PRIVATE_KEY=$(lpass show "Shared-PKS Telemetry/[github] pkstelemetrybot-p
 TOOLSMITHS_API_KEY="$(lpass show "Shared-PKS Telemetry/Toolsmiths API Key" --notes)"
 SLACK_WEBHOOK=$(lpass show "Shared-PKS Telemetry/Slack Webhook" --url)
 
-fly -t wings set-pipeline \
+fly -t hh set-pipeline \
   --config ${DIR}/pipeline.yml \
   --pipeline pks-telemetry-env-renewal \
   --var github-private-key="${GITHUB_PRIVATE_KEY}" \
